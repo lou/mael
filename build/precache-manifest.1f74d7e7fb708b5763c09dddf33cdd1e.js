6cfc0cc1c7e05445e659db6f95bd0672941ea954
lou <louiscuny@gmail.com>
2019-02-23 19:31:01 +0100

    Production build

---
+++ build/precache-manifest.1f74d7e7fb708b5763c09dddf33cdd1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "021b3a6462e017375ce6",
+    "url": "/static/css/main.b8c145f9.chunk.css"
+  },
+  {
+    "revision": "021b3a6462e017375ce6",
+    "url": "/static/js/main.021b3a64.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "0e65b85b34ade88e62ca",
+    "url": "/static/css/2.766c1acb.chunk.css"
+  },
+  {
+    "revision": "0e65b85b34ade88e62ca",
+    "url": "/static/js/2.0e65b85b.chunk.js"
+  },
+  {
+    "revision": "2c69f090d488a106d69293b233eb4845",
+    "url": "/static/media/logo.2c69f090.png"
+  },
+  {
+    "revision": "9d64949e623037072765260d80cd407f",
+    "url": "/index.html"
+  }
+];
