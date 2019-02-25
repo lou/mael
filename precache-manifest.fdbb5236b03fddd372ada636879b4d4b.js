6a9e8bb557eebcb6064e469c8c6c01e724641a25
lou <louiscuny@gmail.com>
2019-02-25 18:42:28 +0100

    Updates

--- precache-manifest.fdbb5236b03fddd372ada636879b4d4b.js
+++ precache-manifest.fdbb5236b03fddd372ada636879b4d4b.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "59edc66b42d51926a686",
+    "revision": "3a1ecdd972845f5584f6",
     "url": "/mael/static/css/main.ba6447cb.chunk.css"
   },
   {
-    "revision": "59edc66b42d51926a686",
-    "url": "/mael/static/js/main.59edc66b.chunk.js"
+    "revision": "3a1ecdd972845f5584f6",
+    "url": "/mael/static/js/main.3a1ecdd9.chunk.js"
   },
   {
     "revision": "10f64e82134931e92fb1",
     "url": "/mael/static/js/runtime~main.10f64e82.js"
   },
   {
-    "revision": "8b69da09b70fb744df22",
-    "url": "/mael/static/js/2.8b69da09.chunk.js"
+    "revision": "60472f279cd0957ff4a6",
+    "url": "/mael/static/js/2.60472f27.chunk.js"
   },
   {
     "revision": "2c69f090d488a106d69293b233eb4845",
     "url": "/mael/static/media/logo.2c69f090.png"
   },
   {
-    "revision": "ab58ef5b94b7e61c6b3f71d42837f1f3",
+    "revision": "bf6cecb7e11c969d0339e812dc36dce1",
     "url": "/mael/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mael/precache-manifest.c50b8da4f34d5eba497b2b70bcf18dcd.js"
+  "/mael/precache-manifest.fdbb5236b03fddd372ada636879b4d4b.js"
 );
 
 workbox.clientsClaim();
